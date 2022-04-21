4ce320ad5e9acccde6e2f408f79eb1f1db54ecca
advisorrry <sypikeh@yandex.com>
2022-04-21 22:57:22 +0500

    подчеркивание надписи в зависимости от того, на какой странцие находится пользователь

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,9 +1,11 @@
 import cn from 'classnames'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Container } from '../../shared/ui/container'
 import styles from './styles.module.scss'
 
 export const Header = () => {
+  const loc = useLocation()
+
   const actions = [
     {
       id: 'films',
@@ -29,14 +31,16 @@ export const Header = () => {
 
   return (
     <Container>
-      <header className="flex items-center justify-center flex-wrap bg-teal xs:justify-between ">
+      <header className="flex items-center justify-center flex-wrap bg-teal sm:justify-between ">
         <div className="font-mono text-xl font-semibold tracking-wider text-teal-500">
           <Link to="/">prisma_movie</Link>
         </div>
         <nav className={cn(styles.root, 'flex font-semibold text-xl lg:text-lg')}>
           {actions.map(({ id, label, url }) => (
-            <Link to={url} className="mr-5 lg:mr-10 last:mr-0" key={id}>
-              <span>{label}</span>
+            <Link to={url} className="mr-3  md:mr-10 last:mr-0" key={id}>
+              <div className={loc.pathname === url ? styles.underline : null}>
+                <span>{label}</span>
+              </div>
             </Link>
           ))}
         </nav>
