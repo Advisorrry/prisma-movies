94cd018a5b116221f6dbdf1e5e45957ba69ed203
advisorrry <sypikeh@yandex.com>
2022-04-21 22:55:41 +0500

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import React, {lazy} from 'react'
+import React, { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Layout } from '../shared/ui/layout/layout'
 import { Header } from '../widgets/header'
 import { Options } from '../widgets/options-group'
 
-const FavoritesPage = lazy(() => import("./favorites-page"));
-const SeriesPage = lazy(() => import("./series-page"));
-const FilmsPage = lazy(() => import("./films-page"));
-const TopPage = lazy(() => import("./top-page"));
+const FavoritesPage = lazy(() => import('./favorites-page'))
+const SeriesPage = lazy(() => import('./series-page'))
+const FilmsPage = lazy(() => import('./films-page'))
+const TopPage = lazy(() => import('./top-page'))
 
-
-export const Routing  = () => {
+export const Routing = () => {
   return (
     <div className="App">
       <Header />
