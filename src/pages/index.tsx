ed7c9608e19b9e0573638338acec0e662c138a50
advisorrry <sypikeh@yandex.com>
2022-04-28 21:54:39 +0500

    add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ const FavoritesPage = lazy(() => import('./favorites-page'))
 const SeriesPage = lazy(() => import('./series-page'))
 const FilmsPage = lazy(() => import('./films-page'))
 const TopPage = lazy(() => import('./top-page'))
+const Footer = lazy(() => import('../widgets/footer'))
 
 export const Routing = () => {
   return (
@@ -20,6 +21,7 @@ export const Routing = () => {
         <Route path="/favorites" element={<FavoritesPage />} />
         <Route path="/top" element={<TopPage />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
