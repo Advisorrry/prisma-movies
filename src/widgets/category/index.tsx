cada1d0025654172d13560c6824e741cf23afbcb
advisorrry <sypikeh@yandex.com>
2022-05-31 17:20:26 +0500

    create component with which can change the categories

---
+++ src/widgets/category/index.tsx
@@ -0,0 +1,42 @@
+import { Container } from '../../shared/ui/container'
+import cn from 'classnames'
+
+enum Page {
+  film = 'film',
+  tv = 'tv',
+}
+interface Props {
+  items: { name: string; cat: string }[]
+  setCategory: (c: string) => void
+  category: string
+  page: string
+}
+
+export const CategoryGroup = ({ items, setCategory, category, page }: Props) => {
+  return (
+    <Container>
+      <div className="flex justify-center items-center mt-5 md:justify-start">
+        {items.map((action, idx) => (
+          <button
+            key={idx}
+            onClick={() => setCategory(action.cat)}
+            className="
+              relative inline-flex items-center mr-2 justify-center p-0.5 overflow-hidden text-base font-medium text-gray-900 rounded-2xl group bg-gradient-to-br from-purple-700 to-blue-500  hover:text-white">
+            <span
+              className={cn(
+                'h-full items-center py-2  transition-all ease-in duration-75 bg-white  rounded-2xl group-hover:bg-opacity-0',
+                {
+                  'bg-gradient-to-br from-purple-700 to-blue-500 text-white':
+                    action.cat === category,
+                  'px-4 xs:px-7': page === Page.tv,
+                  'px-1 xs:px-5': page === Page.film,
+                },
+              )}>
+              {action.name}
+            </span>
+          </button>
+        ))}
+      </div>
+    </Container>
+  )
+}
