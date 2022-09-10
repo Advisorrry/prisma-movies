98c902132f7694eabff1a06979e8a0493b229e7d
advisorrry <sypikeh@yandex.com>
2022-09-10 15:26:32 +0500

    add shadows and bg for information about movie

--- src/shared/ui/movie-details/series-details/index.tsx
+++ src/shared/ui/movie-details/series-details/index.tsx
@@ -21,22 +21,32 @@ export const SeriesDetailsTemplate = ({
 }: Props) => {
   return (
     <div className="text-lg">
-      <div className="flex flex-col md:flex-row justify-items-start   ">
+      <div className="flex flex-col md:flex-row justify-items-start">
         <div className="flex items-center justify-center md:mr-9">
           <img className="w-64 h-96 rounded-lg" src={posterPath} alt={name} />
         </div>
         <div className="mb-2 mt-2 w-full lg:w-2/3">
-          <h2 className="text-2xl subpixel-antialiased font-bold mb-1 justify-center">{name}</h2>
-          <div className="flex w-full sm:w-2/3 items-center justify-between">
-            <span className="font-semibold">Рейтинг: {rating}</span>
-            <span>{genre?.[0].name}</span>
-            <span>Сезонов: {numberOfSeasons}</span>
+          <h2 className="flex text-2xl subpixel-antialiased font-bold mb-1 justify-center">
+            {name}
+          </h2>
+          <div className="flex sm:w-2/3 items-center justify-between mb-4">
+            <span className="font-semibold  drop-shadow-xl bg-white p-2 rounded-lg ">
+              рейтинг: {rating}
+            </span>
+            <span className="drop-shadow-xl bg-white p-2 rounded-lg">{genre?.[0].name}</span>
+            <span className="drop-shadow-xl bg-white p-2 rounded-lg">
+              сезонов: {numberOfSeasons}
+            </span>
           </div>
-          <div className="flex flex-col">
-            <span>Первый эпизод: {firstAirDate}</span>
-            <span>Последний эпизод: {lastAirDate}</span>
+          <div className="inline-block mb-4">
+            <div className="drop-shadow-xl bg-white p-2 rounded-lg mb-2">
+              Первый эпизод: {firstAirDate}
+            </div>
+            <div className="drop-shadow-xl bg-white p-2 rounded-lg">
+              Последний эпизод: {lastAirDate}
+            </div>
           </div>
-          <div className="text-xl w-full sm:w-3/4">
+          <div className="text-xl w-full sm:w-3/4 drop-shadow-xl bg-white p-4 rounded-lg">
             <h5 className="my-2">Описание: </h5>
             <div className="text-xl leading-7">{overview}</div>
           </div>
